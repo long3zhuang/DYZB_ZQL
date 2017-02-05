e458a14905caa99cd8d648407f4ddcaeeb859040
周泉龙 <392234675@qq.com>
2017-02-05 11:19:16 +0800

    首页导航栏

--- DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
+++ DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
@@ -12,24 +12,45 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// MARK:- 设置UI界面
+extension HomeViewController {
+    
+     func setupUI() {
+    
+        setUpNavagationBar()
     }
     
+    // 创建导航栏上子控件
+    private func setUpNavagationBar() {
+        
 
-    /*
-    // MARK: - Navigation
+        // 左侧logo
+        /*
+        // 原始方法
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "logo"), for: .normal)
+        btn.sizeToFit()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
+ */
+//        构造函数
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imgaeName: "logo")
+        
+        // 封装的类方法
+        let size = CGSize(width: 40, height: 40)
+        
+//        let historyButton = UIBarButtonItem.creatItem(imgaeName: "image_my_history", highImageName: "Image_my_history_click", size: size)
+//        let searchButton = UIBarButtonItem.creatItem(imgaeName: "btn_search", highImageName: "btn_search_clicked", size: size)
+//        let qrcodeButton = UIBarButtonItem.creatItem(imgaeName: "Image_scan", highImageName: "Image_scan_click", size: size)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        // 封装的构造方法
+        let historyButton = UIBarButtonItem(imgaeName: "image_my_history", highImageName: "Image_my_history_click", size: size)
+        let searchButton = UIBarButtonItem(imgaeName: "btn_search", highImageName: "btn_search_clicked", size: size)
+        let qrcodeButton = UIBarButtonItem(imgaeName: "Image_scan", highImageName: "Image_scan_click", size: size)
+        navigationItem.rightBarButtonItems = [historyButton, searchButton, qrcodeButton]
     }
-    */
-
 }
--- DouYuZB/DouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
+++ DouYuZB/DouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2017年 love阿木蛋花. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIBarButtonItem {
+// 扩展一个类方法
+    class func creatItem(imgaeName :String ,highImageName : String, size : CGSize) ->UIBarButtonItem {
+        let btn = UIButton()
+        
+        btn.setImage(UIImage(named:imgaeName), for: .normal)
+        btn.setImage(UIImage(named:highImageName), for: .highlighted)
+        
+        btn.frame = CGRect(origin: CGPoint.zero, size: size)
+        
+        return UIBarButtonItem(customView: btn)
+    }
+// 扩展一个构造方法 需要两个条件1.只能在extention中构造便利构造函数,以convenience开头 
+//                        2.在构造函数中必须明确调用一个设计的构造函数(self)
+     convenience init(imgaeName :String ,highImageName: String = "", size: CGSize = CGSize.zero) {
+        
+        let btn = UIButton()
+        // 设置button图片
+        btn.setImage(UIImage(named:imgaeName), for: .normal)
+        if highImageName != "" {
+            btn.setImage(UIImage(named:highImageName), for: .highlighted)
+        }
+        // 设置button尺寸
+        if size == CGSize.zero {
+            btn.sizeToFit()
+        }else {
+            btn.frame = CGRect(origin: CGPoint.zero, size: size)
+        }
+        
+        self.init(customView : btn)
+    }
+}
